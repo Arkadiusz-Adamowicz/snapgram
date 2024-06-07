73c67a782b2e230dc308a24cb0955a428f0c1dec
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-06-07 21:36:22 +0200

    update

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
 
   return (
     <div className='flex flex-1'>
-      <div className='home-container'>
+      <div className='home-container no-scrollbar'>
         <div className='home-posts'>
           <h2 className='h3-bold md:h2-bold w-full flex items-center'>
             <img
