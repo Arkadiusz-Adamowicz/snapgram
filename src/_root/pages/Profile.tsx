6dba53ceb6fbccecf4a32a132fb1dde007570836
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-06-09 02:48:53 +0200

    update

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -3,7 +3,14 @@ const Profile = () => {
     <div className='flex flex-1'>
       <div className='common-container'>
         <div className='flex-start gap-3 justify-start w-full'>
-          <h2 className='h3-bold md:h2-bold w-full'>Profile</h2>
+          <h2 className='h3-bold md:h2-bold w-full flex items-center mb-5'>
+            <img
+              src='/assets/icons/follow.svg'
+              alt='saved'
+              className='invert-white mr-2 md:h-8 md:w-8 h-6 w-6'
+            />
+            Profile
+          </h2>
         </div>
       </div>
     </div>
