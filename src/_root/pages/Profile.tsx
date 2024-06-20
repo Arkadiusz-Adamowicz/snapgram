ef155496de82ecf3d0f0de056456f9226ec64c1b
Arek <arek.adamowicz81@gmail.com>
2024-06-20 09:51:23 +0200

    update

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -4,19 +4,19 @@ import {
   Link,
   Outlet,
   useParams,
-  useLocation,
-} from 'react-router-dom';
+  useLocation
+} from 'react-router-dom'
 
-import { LikedPosts } from '@/_root/pages';
-import { useUserContext } from '@/context/AuthContext';
-import Loader from '@/components/shared/Loader';
-import { Button } from '@/components/ui/button';
-import GridPostList from '@/components/shared/GridPostList';
-import { useGetUserById } from '@/lib/react-query/queriesAndMutations';
+import { LikedPosts } from '@/_root/pages'
+import { useUserContext } from '@/context/AuthContext'
+import Loader from '@/components/shared/Loader'
+import { Button } from '@/components/ui/button'
+import GridPostList from '@/components/shared/GridPostList'
+import { useGetUserById } from '@/lib/react-query/queriesAndMutations'
 
 interface StabBlockProps {
-  value: string | number;
-  label: string;
+  value: string | number
+  label: string
 }
 
 const StatBlock = ({ value, label }: StabBlockProps) => (
@@ -24,94 +24,97 @@ const StatBlock = ({ value, label }: StabBlockProps) => (
     <p className='small-semibold lg:body-bold text-primary-500'>{value}</p>
     <p className='small-medium lg:base-medium text-light-2'>{label}</p>
   </div>
-);
+)
 
 const Profile = () => {
-  const { id } = useParams();
-  const { user } = useUserContext();
-  const { pathname } = useLocation();
+  const { id } = useParams()
+  const { user } = useUserContext()
+  const { pathname } = useLocation()
 
-  const { data: currentUser } = useGetUserById(id || '');
+  const { data: currentUser } = useGetUserById(id || '')
 
   if (!currentUser)
     return (
-      <div className='flex-center w-full h-full'>
+      <div className='flex-center h-full w-full'>
         <Loader />
       </div>
-    );
+    )
 
   return (
     <div className='profile-container'>
       <div className='profile-inner_container'>
-        <div className='flex-col gap-3 justify-start w-full'>
-          <h2 className='h3-bold md:h2-bold w-full flex items-center mb-5'>
+        <div className='w-full flex-col justify-start gap-3'>
+          <h2 className='h3-bold md:h2-bold mb-5 flex w-full items-center'>
             <img
               src='/assets/icons/follow.svg'
               alt='saved'
-              className='invert-white mr-2 md:h-8 md:w-8 h-6 w-6'
+              className='invert-white mr-2 h-6 w-6 md:h-8 md:w-8'
             />
             Profile
           </h2>
-          <div className='flex xl:flex-row flex-col max-xl:items-center flex-1 gap-7'>
+          <div className='flex flex-1 flex-col gap-7 max-xl:items-center xl:flex-row'>
             <img
               src={
                 currentUser.imageUrl || '/assets/icons/profile-placeholder.svg'
               }
               alt='profile'
-              className='w-28 h-28 lg:h-36 lg:w-36 rounded-full object-cover'
+              className='h-28 w-28 rounded-full object-cover lg:h-36 lg:w-36'
             />
-            <div className='flex flex-col flex-1 justify-between md:mt-2'>
-              <div className='flex flex-col w-full'>
-                <h1 className='text-center xl:text-left h3-bold md:h1-semibold w-full'>
-                  {currentUser.name}
-                </h1>
-                <p className='small-regular md:body-medium text-light-3 text-center xl:text-left'>
-                  @{currentUser.username}
-                </p>
+            <div className='flex flex-1 flex-col justify-between md:mt-2'>
+              <div className='fle flex w-full max-w-screen-sm flex-col items-center justify-between gap-5 xl:flex-row'>
+                <div>
+                  <h1 className='h3-bold md:h1-semibold w-full text-center xl:text-left'>
+                    {currentUser.name}
+                  </h1>
+                  <p className='small-regular md:body-medium text-center text-light-3 xl:text-left'>
+                    @{currentUser.username}
+                  </p>
+                </div>
+                <div className={`${user.id !== currentUser.$id && 'hidden'}`}>
+                  <Link
+                    to={`/update-profile/${currentUser.$id}`}
+                    className={`flex-center h-12 gap-2 rounded-lg bg-dark-4 px-5 text-light-1 ${
+                      user.id !== currentUser.$id && 'hidden'
+                    }`}
+                  >
+                    <img
+                      src={'/assets/icons/edit.svg'}
+                      alt='edit'
+                      width={20}
+                      height={20}
+                    />
+                    <p className='small-medium flex whitespace-nowrap'>
+                      Edit Profile
+                    </p>
+                  </Link>
+                </div>
+
+                <div className={`${user.id === id && 'hidden'}`}>
+                  <Button
+                    type='button'
+                    className='shad-button_primary max-w-screen-sm px-8'
+                  >
+                    Follow
+                  </Button>
+                </div>
               </div>
 
-              <div className='flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20'>
+              <div className='z-20 mt-10 flex w-full flex-wrap items-center justify-center gap-8 xl:justify-start'>
                 <StatBlock value={currentUser.posts.length} label='Posts' />
                 <StatBlock value={20} label='Followers' />
                 <StatBlock value={20} label='Following' />
               </div>
 
-              <p className='small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm'>
+              <p className='small-medium md:base-medium mt-7 max-w-screen-sm text-center xl:text-left'>
                 {currentUser.bio}
               </p>
-            </div>
-
-            <div className='flex justify-center gap-4'>
-              <div className={`${user.id !== currentUser.$id && 'hidden'}`}>
-                <Link
-                  to={`/update-profile/${currentUser.$id}`}
-                  className={`h-12 bg-dark-4 px-5 text-light-1 flex-center gap-2 rounded-lg ${
-                    user.id !== currentUser.$id && 'hidden'
-                  }`}
-                >
-                  <img
-                    src={'/assets/icons/edit.svg'}
-                    alt='edit'
-                    width={20}
-                    height={20}
-                  />
-                  <p className='flex whitespace-nowrap small-medium'>
-                    Edit Profile
-                  </p>
-                </Link>
-              </div>
-              <div className={`${user.id === id && 'hidden'}`}>
-                <Button type='button' className='shad-button_primary px-8'>
-                  Follow
-                </Button>
-              </div>
             </div>
           </div>
         </div>
       </div>
 
       {currentUser.$id === user.id && (
-        <div className='flex max-w-5xl w-full'>
+        <div className='flex w-full'>
           <Link
             to={`/profile/${id}`}
             className={`profile-tab rounded-l-lg ${
@@ -154,7 +157,7 @@ const Profile = () => {
       </Routes>
       <Outlet />
     </div>
-  );
-};
+  )
+}
 
-export default Profile;
+export default Profile
